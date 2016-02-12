d7d30f1c3a9ea4fc64b2c0585ac4753433330bbc
pcollender <pcollender@berkeley.edu>
2016-02-11 17:16:54 -0800

    Update Biphasic_non_linear_least_squares_regression.R
    Various workflow improvements

--- Biphasic_non_linear_least_squares_regression.R
+++ Biphasic_non_linear_least_squares_regression.R
@@ -8,36 +8,49 @@
 #Per the terms of this license, if you are making derivative use of this work, you must identify that your work is a derivative work, 
 #give credit to the original work, provide a link to the license, and indicate changes that were made.
 
+#File is saved as Cerf_Regression.R
+
+CPest=function(alpha,k1,k2){
+  log(alpha/(1-alpha))/(k1-k2)
+}#Function to estimate changepoint time coordinate if nls trace is available but function doesnt converge
+
 CerfReg=function(Data,Exper){
   layout(1)
-  Time = Data$Time[Data$Exp==Exper]
-  Conc = Data$ln_Conc[Data$Exp==Exper]
-  plot(Time,Conc)
+  Time <- Data$Time[Data$Exp==Exper]
+  lnConc <<- Data$ln_Conc[Data$Exp==Exper]
+  plot(Time,lnConc)
   retry=1
   while(retry>0){  
     retry=retry-1
-    Yest<-as.double(readline(prompt="At what Y value does the change in slope appear to take place? "))
-    Xest<-as.double(readline(prompt="At what X value does the change in slope appear to take place? "))
-    Xvals<<-c(max(Time[Time<Xest]),min(Time[Time>Xest]))
-    Yvals<<-c(Conc[Time==Xvals[1]],Conc[Time==Xvals[2]])
+    Yest<-eval(parse(text=readline(prompt="At what Y value does the change in slope appear to take place? (Hit Enter if no changepoint is visible)")))
+    Xest<-eval(parse(text=readline(prompt="At what X value does the change in slope appear to take place? (Hit Enter if no changepoint is visible)")))
+    Xvals<-c(max(Time[Time<Xest]),min(Time[Time>Xest]))
+    Yvals<-c(lnConc[Time==Xvals[1]],lnConc[Time==Xvals[2]])
     
-    K1init<<--(Conc[Time==0]-Yvals[1])/Xvals[1]
+    K1init<-(lnConc[Time==0]-Yvals[1])/Xvals[1]
     if (is.nan(K1init)){
-      K1init<<--(Conc[Time==0]-Yest)/Xest
+      K1init<-(lnConc[Time==0]-Yest)/Xest
     }
     
-    K2init<<--(Yvals[2]-Conc[length(Conc)])/(max(Time)-Xvals[2])
+    K2init<-(Yvals[2]-lnConc[length(lnConc)])/(max(Time)-Xvals[2])
     if (is.nan(K2init)){
-      K2init<<--(Yest-Conc[length(Conc)])/(max(Time)-Xest)
+      K2init<-(Yest-lnConc[length(lnConc)])/(max(Time)-Xest)
     }
-    c=exp((K2init-K1init)*Xest)
-    alphainit<<-c/(c+1)
+    c=exp((K1init-K2init)*Xest)
+    alphainit<-c/(c+1)
     
     
-    saved.fits<<-try(nlsLM(Conc~Y0+log(alpha*exp(K1*Time)+(1-alpha)*exp(K2*Time)), algorithm='LM',
-                           start=list(Y0=Conc[Time==min(Time)], alpha=alphainit, K1=K1init, K2=K2init), lower=c(0,0,-Inf,-Inf), 
+    saved.fits<-try(nlsLM(lnConc~Y0+log(alpha*exp(-K1*Time)+(1-alpha)*exp(-K2*Time)), algorithm='LM',
+                           start=list(Y0=lnConc[Time==min(Time)], alpha=alphainit, K1=K1init, K2=K2init), lower=c(0,0,-Inf,-Inf), 
                            upper=c(Inf,1,Inf,Inf),trace=TRUE))
-    if (length(saved.fits)==1){
+    Check2='n'
+    newdat<-data.frame(seq(Time[1],Time[length(Time)],by=abs(Time[1]-Time[2]/40)))
+    names(newdat)='Time'
+    
+    if (length(saved.fits)==1 & (!is.null(Xest) | !is.null(Yest))){
+      plot(newdat$Time,lnConc[Time==0]+log(alphainit*exp(-K1init*newdat$Time)+(1-alphainit)*exp(-K2init*newdat$Time)),
+           type='l',xlab='Time',ylab='Ln(Concentration)', main=paste('experiment',Exper, sep=' '), 
+           sub=sprintf('INITIAL ESTIMATES'),ylim=c(min(lnConc),max(lnConc)))+points(Time,lnConc)
       Check1=readline(prompt='Retry? (y or n) \n')
       if(Check1=='y'){
         retry=1 
@@ -49,25 +62,29 @@ CerfReg=function(Data,Exper){
   }
   
   
-  linear.fits=lm(Conc~Time)
-  
-  layout(matrix(seq(1:2)))
-  newdat=data.frame(seq(Time[1],Time[length(Time)],by=abs(Time[1]-Time[2]/40)))
-  names(newdat)='Time'
-  if(Check2!='y'){
-    plot(newdat$Time,predict(saved.fits,newdata=newdat),type='l',xlab='Time',ylab='Ln(Concentration)', 
-         main='experiment...', sub=sprintf('model error = %s',summary(saved.fits)$sigma),ylim=c(min(Conc),max(Conc)))+points(Time,Conc)
-    biphsave<<-summary(saved.fits)$coefficients
-  }else{
-    plot(newdat$Time,Conc[Time==0]+log(alphainit*exp(K1init*newdat$Time)+(1-alphainit)*exp(K2init*newdat$Time)),
-         type='l',xlab='Time',ylab='Ln(Concentration)', main='experiment...', 
-         sub=sprintf('initial estimates'),ylim=c(min(Conc),max(Conc)))+points(Time,Conc)
-    biphsave<<-cbind(c(Conc[Time==0],alphainit,K1init,K2init))
-  }
-  
-  plot(newdat$Time, predict(linear.fits,newdata=newdat),type='l',xlab='Time',ylab='Ln(Concentration)',
-       main=Exper, sub=sprintf('model error = %s', summary(linear.fits)$sigma),ylim=c(min(Conc),max(Conc)))+points(Time,Conc)
+  linear.fits=lm(lnConc~Time)
   
   
+  newdat<<-newdat
+  biphsave=NA
+  if (!is.null(Xest) & !is.null(Yest)){
+    layout(matrix(seq(1:2)))
+    if(Check2!='y'){
+      plot(newdat$Time,predict(saved.fits,newdata=newdat),type='l',xlab='Time',ylab='Ln(Concentration)', 
+           main='experiment...', sub=sprintf('model error = %s',summary(saved.fits)$sigma),ylim=c(min(lnConc),max(lnConc)))+points(Time,lnConc)
+      biphsave<-summary(saved.fits)$coefficients
+    }else{
+      plot(newdat$Time,lnConc[Time==0]+log(alphainit*exp(-K1init*newdat$Time)+(1-alphainit)*exp(-K2init*newdat$Time)),
+           type='l',xlab='Time',ylab='Ln(Concentration)', main='experiment...', 
+           sub=sprintf('INITIAL ESTIMATES'),ylim=c(min(lnConc),max(lnConc)))+points(Time,lnConc)
+      biphsave<-cbind(c(lnConc[Time==0],alphainit,K1init,K2init))
+    }
+    plot(newdat$Time, predict(linear.fits,newdata=newdat),type='l',xlab='Time',ylab='Ln(Concentration)',
+         main=Exper, sub=sprintf('model error = %s', summary(linear.fits)$sigma),ylim=c(min(lnConc),max(lnConc)))+points(Time,lnConc)
+  }else(
+    plot(newdat$Time, predict(linear.fits,newdata=newdat),type='l',xlab='Time',ylab='Ln(Concentration)',
+         main=paste('experiment',Exper, sep=' '), sub=sprintf('model error = %s', summary(linear.fits)$sigma),ylim=c(min(lnConc),max(lnConc)))+points(Time,lnConc)
+  )
+
   return(list(saved.fits,biphsave,linear.fits,summary(linear.fits)))
 }
